eb4a97cf55444122087b4834cc47003cbde9d1f6
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-08-28 12:15:40 -0300

    fix

--- config/api.config.ts
+++ config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '/api/proxy',
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
   ENDPOINTS: {
     COMPANY: {
       REQUEST_ACCESS: 'waiting-list',
